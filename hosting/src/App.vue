71f59a14076792aabe0da6007f47d436781a1de4
Aljodomo <26247274+Aljodomo@users.noreply.github.com>
2024-03-17 19:21:40 +0100

    feat: keepalive home

--- hosting/src/App.vue
+++ hosting/src/App.vue
@@ -6,7 +6,13 @@ import NavBar from './components/nav/NavBar.vue'
 <template>
   <div class="flex flex-col h-full justify-stretch">
     <div class="overflow-auto flex-1 flex flex-col">
-      <RouterView />
+      <RouterView v-slot="{ Component }">
+        <template v-if="Component">
+          <KeepAlive include="PlantsOverviewLoader">
+              <component :is="Component" />
+          </KeepAlive>
+        </template>
+      </RouterView>
     </div>
     <NavBar class=""></NavBar>
   </div>
