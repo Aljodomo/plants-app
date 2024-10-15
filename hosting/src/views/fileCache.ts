0f997c2448a96a9415974222cc6adab0eaeec7e2
Aljoscha Domonell <aljoscha.domonell@mercedes-benz.com>
2024-10-15 23:49:00 +0200

    feat: plant images

---
+++ hosting/src/views/fileCache.ts
@@ -0,0 +1,16 @@
+async function getFileCache() {
+  return await window.caches.open('plants-app-files')
+}
+
+export async function cachedBlobUrl(url?: string): Promise<string | undefined> {
+  if (!url) return undefined
+  const request = new URL(url)
+  const fileCache = await getFileCache()
+  const cacheEntry = await fileCache.match(request)
+  if (!cacheEntry) {
+    console.warn(`Cache miss: ${url}`)
+    await fileCache.add(request)
+  }
+  const cacheHit = (await fileCache.match(request))!
+  return URL.createObjectURL(await cacheHit.blob())
+}
---
+++ hosting/src/views/imageRepo.ts
@@ -0,0 +1,31 @@
+import { getDownloadURL, getStorage, ref, uploadBytes, deleteObject } from 'firebase/storage'
+import { type PlantInfo, savePlantInfo } from '@/views/plantsRepo'
+
+const storage = getStorage()
+
+function getRef(url: string) {
+  return ref(storage, url)
+}
+
+function getImageRef(imageName: string) {
+  return getRef(`images/${imageName}`)
+}
+
+async function uploadImage(imageName: string, file: File) {
+  const ref = getImageRef(imageName)
+  return uploadBytes(ref, file)
+}
+
+export async function uploadPlantImage(plantInfo: PlantInfo, file: File) {
+  const res = await uploadImage(plantInfo.id, file)
+  plantInfo.imageUrl = await getDownloadURL(res.ref)
+  await savePlantInfo(plantInfo.id, plantInfo)
+  return plantInfo
+}
+
+export async function deletePlantImage(plantInfo: PlantInfo) {
+  const ref = getImageRef(plantInfo.id)
+  await deleteObject(ref)
+  delete plantInfo.imageUrl
+  await savePlantInfo(plantInfo.id, plantInfo)
+}
